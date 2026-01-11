067da48d5b4c091f95d10269eebe23adb980d919
quocbaodev1608 <zzzzquc@gmail.com>
2026-01-11 21:14:27 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+[
+  {
+    "id": "app1",
+    "name": "Game Hack iOS VIP",
+    "platform": "iOS",
+    "icon": "https://i.imgur.com/8Km9tLL.png",
+    "desc": "Hack full chức năng, mượt",
+    "rating": 5,
+    "downloads": 0,
+    "link": "https://link-ipa.com"
+  }
+]
